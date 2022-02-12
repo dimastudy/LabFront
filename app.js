2266e6ff11dc044c3747a6718e657c663ec763f5
dimastudy <dimastudymail@gmail.com>
2022-02-12 13:34:12 +0200

    init

---
+++ app.js
@@ -0,0 +1,16 @@
+
+let name = ""
+const GOVNO_POST = "CONST_SHIT"
+
+const shit = {
+    shitLength: "3cm",
+    shitSmell: smell = {
+        smellCount: 0,
+        smellFuck: false
+    },
+    shitInfo: function (){
+        alert(this.shitLength + this.shitSmell.smellCount.toString() + this.shitSmell.smellFuck)
+    }
+}
+
+shit.shitInfo()
